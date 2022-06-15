9b7bb4b53e05fc1c06e9023c96a6080efe803b9e
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2022-06-15 11:36:53 -0300

    Add files via upload
    Adicionado chão e comportamento em loop

--- Aula_3.py
+++ Aula_3.py
@@ -6,18 +6,18 @@ window.color = color.white
 
 dino = Animation('assets\dino', collider='box', x=-5)
 
-ground1 = Entity(
+ground1 = Entity(      #criamos o objeto chão, com os parametros de modelo (quadrado), textura, escala e ordem do eixo z
   model='quad',
   texture='assets\ground',
   scale=(50,0.5,1),
   z=1
 )
-ground2 = duplicate(ground1, x=50)
-pair = [ground1, ground2]
+ground2 = duplicate(ground1, x=50) #clone do ground1, porém 50 pontos no eixo x pra direita.
+pair = [ground1, ground2]  #criamos um array de dois chãos que ficam lado a lado para que possam se repetir
 
 def update():
   for ground in pair:
-    ground.x -= 6*time.dt
+    ground.x -= 6*time.dt #chao se move para a esquerda no eixo X ao ser subtraído pela velocidade, essa que é determinada pelo tempo. Ela Pode ser multiplicada, no caso, por 6
     if ground.x < -35:
       ground.x += 100
 
